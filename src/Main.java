565dffd3931f8a8fff069a16a9afd4c1d91d7b7e
Prayash Shrestha <xrestha17@gmail.com>
2024-06-25 13:40:11 -0500

    learning about fundamentals

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Honda myHonda = new Honda("CRV 2022");
-        myHonda.drive();
-        Honda myToyota = new Honda("RAV4 2022");
-        myToyota.drive();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number?");
+        String a = scanner.nextLine();
+        System.out.println("Enter second number?");
+        String b = scanner.nextLine();
+
+        int sum = Integer.parseInt(a) + Integer.parseInt(b);
+        int product = Integer.parseInt(a) * Integer.parseInt(b);
+
+        float test = Float.parseFloat(sum + Integer.toString(product));
+
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(test);
     }
 }
