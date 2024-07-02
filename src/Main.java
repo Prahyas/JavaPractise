14e9013aa228bb1784d3b05c3d2eb0a88d7cb021
Prayash Shrestha <xrestha17@gmail.com>
2024-07-01 22:01:39 -0500

    learning OOPS with examples

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
     public static void main(String[] args) {
         Honda honda = new Honda("Honda CRV", 4);
         honda.displayCar();
-        
+        // this is a comment
     }
 }
