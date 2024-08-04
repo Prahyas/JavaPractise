9ce89d7ae6597789bb1a47af32fdf6206ca6ab21
Prayash Shrestha <xrestha17@gmail.com>
2024-08-04 15:55:17 -0500

    learning coding problems

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
 
 
     public static void main(String[] args) {
         try {
-            String currentPath = System.getProperty("user.div");
-            File file = new File(currentPath, "src/logs.txt");
-            Scanner scanner = new Scanner(file);
 
-            while (scanner.hasNextLine()) {
-                String input = scanner.nextLine();
-                System.out.println(input);
+            int[][] arr1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+
+            int[][] result = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    result[j][i] = arr1[i][j];
+                }
             }
-            scanner.close();
-        } catch (IOException e) {
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.println(result[i][j]);
+                }
+            }
+
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
 
     }
 
