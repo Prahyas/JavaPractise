ec5c4c2309a79c4b70b9430ae8bb36dba135c3ad
Prayash Shrestha <xrestha17@gmail.com>
2024-08-04 11:35:07 -0500

    learning coding problems

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        String[] myString = {"abc", "cb", "a"};
-        String result = longest(3, myString);
-        System.out.println(result);
-    }
+        try {
+            String currentPath = System.getProperty("user.div");
+            File file = new File(currentPath, "src/logs.txt");
+            Scanner scanner = new Scanner(file);
 
-
-    // started again after long gap
-    public static String longest(int n, String[] names) {
-        String longestName = names[0];
-
-        for (int i = 1; i < n; i++) {
-            if (names[i].length() > longestName.length()) {
-                longestName = names[i];
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                System.out.println(input);
             }
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
-        return longestName;
     }
+
 }
