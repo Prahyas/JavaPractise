229a131b386176179d964d20a72dabb7aae87549
Prayash Shrestha <xrestha17@gmail.com>
2024-08-18 14:55:58 -0500

    learning pass by value and pass by refrence

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,19 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-
-            int[][] arr1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-
-            int[][] result = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    result[j][i] = arr1[i][j];
-                }
-            }
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.println(result[i][j]);
-                }
-            }
-
+            Person person = new Person();
+            person.name = "Prayash";
+            changeName(person);
+            System.out.println(person.name);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
+    }
 
+    static void changeName(Person person) {
+        person.name = "Ashmita";
     }
 
 }
